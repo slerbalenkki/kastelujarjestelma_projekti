f6ecf393441b3cc0eb9fe67c9cb54da690c460a6
Mikko Harjama <mikko.harjama@ictpro.labra>
2018-11-28 13:09:56 +0200

    Näyttö lisätty + koodia viilattu

--- kastelujarjestelma_projekti.ino
+++ kastelujarjestelma_projekti.ino
@@ -1,14 +1,23 @@
+#include <LiquidCrystal.h>
+
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+
 int LEDON = LOW;
 int LEDOFF = HIGH;
 
 int sensorPin = A0;       //Kosteusanturin pinni 
 int led = 13;             //Ledin pinni
 int thresholdUp = 400;    //Kosteusarvon yläraja -> pumppu kiinni
-int thresholdDown = 300;  //Kosteusarvon alaraja -> pumppu päälle
+int thresholdDown = 250;  //Kosteusarvon alaraja -> pumppu päälle
 
 void setup() {
   Serial.begin(9600); 
-  pinMode(led, OUTPUT); 
+  pinMode(led, OUTPUT);
+  lcd.begin(16, 2);
+  lcd.print("Hello beautiful!"); 
+  delay(2000);
 }
 
 void loop() {
@@ -22,12 +31,16 @@ void loop() {
   if(sensorValue >= thresholdDown){
     
     //Määritetään viesti ja vilkku, kun kasvi tarvitsee vettä
-    DisplayWords = "Kasvilla on jano!";                             
+    DisplayWords = "Kasvilla jano!";                             
     digitalWrite(led, LEDON);   //Ledi päälle
-      delay(500);               //Sekunnin viive 
+      delay(500);               //Viive 
     digitalWrite(led, LEDOFF);  //Ledi pois päältä
-      delay(500);               //Sekunnin viive
+      delay(500);               //Viive
     Serial.print(DisplayWords); //Viesti muistutukseksi
+    lcd.setCursor(0, 0);
+    lcd.print(DisplayWords);
+    lcd.setCursor(0, 1);
+    lcd.print(sensorValue);
   
   }else if (sensorValue <= thresholdUp){
 
@@ -35,12 +48,16 @@ void loop() {
     DisplayWords = "Jano sammutettu!";
     Serial.print(DisplayWords);
     digitalWrite(led, LEDOFF);  //Ledi pois päältä, kun kasvi ei tarvitse vettä
+    lcd.setCursor(0, 0);
+    lcd.print(DisplayWords);
+    lcd.setCursor(0, 1);
+    lcd.print(sensorValue);
   }else{
 
     Serial.print(DisplayWords);
         
   }
 
-  delay(500); //Puolen sekunnin viive lukemisen helpottamiseksi
+  delay(500); //Viive lukemisen helpottamiseksi
 
 }
